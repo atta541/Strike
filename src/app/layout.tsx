445c3e37e8c3de81abd0463222c7575b85633559
atta541 <attareh542@gmail.com>
2025-02-13 02:42:29 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1F1F21]`}
-      > */}
-
-
 <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1F1F21] min-h-screen`}>
 
         <Bar />
